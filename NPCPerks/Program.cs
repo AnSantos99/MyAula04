fa56008cf8a7660f2c2804b7f0385b7c5de3ad13
AnSantos99 <44712120+AnSantos99@users.noreply.github.com>
2019-03-22 11:49:59 +0000

    NPC Perks using enum

--- NPCPerks/Program.cs
+++ NPCPerks/Program.cs
@@ -6,7 +6,60 @@ namespace NPCPerks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //declarar variaveis
+            int NumNpcs;
+            int npcCarac;
+            Caracteristicas[] caract;
+            char answer;
+            
+
+
+            //Perguntar npcs ao utilizador
+            Console.WriteLine("How many Npc's do you want");
+            NumNpcs = Convert.ToInt32(Console.ReadLine());
+
+            caract = new Caracteristicas[NumNpcs];
+            // ciclo for
+            for (int i = 0; i < NumNpcs; i++)
+            {
+                
+                for (int i = 0; i <= NumNpcs; i++)
+                {
+
+                    Console.WriteLine("Would you like to have Stealth?");
+                    
+                    answer = Convert.ToChar(Console.ReadLine());
+
+                    if (answer == 'y')
+                    {
+                        caract[i] |= Caracteristicas.Stealth;
+                    }
+                    Console.WriteLine("Would you like to have Combat?");
+                    else if (answer == 'y')
+                    {
+                        caract[i] |= Caracteristicas.Combat;
+                    }
+                    else if (answer == 'y')
+                    {
+                        caract[i] |= Caracteristicas.Lockpick;
+                    }
+                    else if (answer == 3)
+                    {
+                        caract[i] |= Caracteristicas.Luck;
+                    }
+
+                }
+                
+            }
+
+            for (int i = 0; i < NumNpcs; i++)
+            {
+                Console.WriteLine(caract[i]);
+            }
+            //guardar characteristicas num array
+
+            //Imprimir Npcs mais caracteristica
+
         }
-    }
+}
 }
